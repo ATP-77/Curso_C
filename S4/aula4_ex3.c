4ec226a6627b1f531e9889c6f66ad4e32718956e
ATP-77 <andre.piarini@gmail.com>
2024-01-23 21:53:35 -0300

    exercicio 3 final

--- S4/aula4_ex3.c
+++ S4/aula4_ex3.c
@@ -8,7 +8,10 @@ apostou, o valor do prêmio e imprima quanto cada um ganharia do prêmio com bas
 
 int main() {
 
-    float valor1, valor2, valor3, premio;
+    float valor1, valor2, valor3, total =0;
+    float premio =0;
+    float porcentagem1, porcentagem2, porcentagem3, porcentagem_total =0;
+    float premio1, premio2, premio3, total_premios=0;
 
     printf("Valor apostado pelo primeiro jogador: ");
     scanf("%f", &valor1);
@@ -19,8 +22,32 @@ int main() {
     printf("Valor apostado pelo terceiro jogador: ");
     scanf("%f", &valor3);
 
-    
+    total = (float)(valor1 + valor2 + valor3);
+    printf("somatória das apostas dos jogadores %.2f\n", total);
 
+    printf("Insira o valor do Prêmio do jogo: ");
+    scanf("%f", &premio);
 
+    porcentagem1 = (float)(valor1 / total);
+    printf("Porcentual do apostador 01: %.3f\n", porcentagem1);
 
+    porcentagem2 = (float)(valor2 / total);
+    printf("Porcentual do apostador 01: %.3f\n", porcentagem2);
+
+    porcentagem3 = (float)(valor3 / total);
+    printf("Porcentual do apostador 01: %.3f\n", porcentagem3);
+
+    premio1 =(float)(premio * porcentagem1);
+    printf("Parte do premio apostador 01: %.2f\n", premio1);
+
+    premio2 =(float)(premio * porcentagem2);
+    printf("Parte do premio apostador 01: %.2f\n", premio2);
+
+    premio3 =(float)(premio * porcentagem3);
+    printf("Parte do premio apostador 01: %.2f\n", premio3);
+
+    total_premios = (float)(premio1 + premio2 + premio3);
+    printf("Somatório após partilha: %.3f \n", total_premios);
+
+    return 0;
 }
