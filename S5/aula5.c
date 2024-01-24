c80d7dc92b4f9e45ccda81349246f178e6840df3
ATP-77 <andre.piarini@gmail.com>
2024-01-23 22:58:18 -0300

    Inicio S5

---
+++ S5/aula5.c
@@ -0,0 +1,26 @@
+// Estruturas de reptição: For, While, do while
+
+#include <stdio.h>
+
+int main () {
+
+    //Variaveis de entrada:
+
+    int numero, soma =0;
+    printf("informe um numero: ");
+    scanf("%d", &numero);
+
+    // Processamento:
+
+    soma = soma + numero;
+
+
+
+    //Saída:
+
+    printf("A Soma é: %d", soma);
+
+
+
+    return 0;
+}
