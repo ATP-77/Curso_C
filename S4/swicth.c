6e08e3c3d093c738219045e1fd9c26acbae4dc98
ATP-77 <andre.piarini@gmail.com>
2024-01-23 22:34:09 -0300

    Exemplo swicth

---
+++ S4/swicth.c
@@ -0,0 +1,43 @@
+//Estruturas de decisão swicth:
+
+#include <stdio.h>
+
+int main() {
+    int valor;
+
+    printf("Digite um valor de 1 a 7:\n");
+    scanf("%d", &valor);
+
+    switch (valor)
+    {
+    case  1 /* constant-expression */:
+        /* code */
+        printf("Domingo!\n");
+        break;
+    case 2:
+        printf("Segunda!\n");
+        break;
+    case 3:
+        printf("Terça!\n");
+        break;
+    case 4:
+        printf("Quarta!\n");
+        break;
+    case 5:
+        printf("Quinta!\n");
+        break;
+    case 6:
+        printf("Sexta\n");
+        break;
+    case 7:
+        printf("Sabado!\n");
+        break;
+
+    
+    default:
+        printf("Valor invalido!");
+        break;
+    }
+
+    return 0;
+}
