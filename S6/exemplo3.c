f691337ce57c3639d58f672a8f48f3620dbec288
ATP-77 <andre.piarini@gmail.com>
2024-01-25 16:33:58 -0300

    exemplo3 - caracteres

---
+++ S6/exemplo3.c
@@ -0,0 +1,20 @@
+/*Tipos de dados em C:  -> Alfanuméricos: Caracteres e strings*.
+// Na linguagem C não exite o tipo de dado string.  ("dfjsjfslk" -> String), 
+('Caracter' -> caracter)
+*/
+
+#include <stdio.h>
+
+
+int main () {
+
+    char nome[50];
+
+    printf("Qual é o seu nome? \n");
+    //gets(nome);  // Funcao insegura  -> fgets
+    fgets(nome, 49, stdin);  
+
+    printf("Seu nome é %s\n", nome);
+
+    return 0;
+}
