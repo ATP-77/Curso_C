89fade5e74dba4cb49494b3cfcc217b6b53f3a91
ATP-77 <andre.piarini@gmail.com>
2024-01-25 13:13:50 -0300

    correcao ex3

--- S5/aula5_ex3.c
+++ S5/aula5_ex3.c
@@ -4,6 +4,33 @@
 
 int main () {
 
+    int numero, maior, menor = 0;
 
-    
+    for (int i =0; i <10; i++) {
+        
+        printf("Informe o valor %d de 10: ", i + 1);
+        scanf("%d", &numero);
+        
+
+        // No primeiro Loop:
+        if (i == 0){
+            maior = numero;
+            menor = numero;
+        }
+
+        // Loops seguintes...
+        // Maior:
+        if (numero > maior){
+            maior =numero;
+        }
+        // Menor:
+        if (numero < menor) {
+            menor = numero;
+        }
+
+    }
+
+    printf("O maior valor é %d e o menor é %d", maior, menor);
+
+    return 0;
 }
